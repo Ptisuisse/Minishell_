c9c0820678c4adf418708cf49e2ac8c287987aac
lisambet <isambetova@gmail.com>
2024-10-15 11:08:37 +0200

    export is working

--- env_var.c
+++ env_var.c
@@ -19,29 +19,29 @@ char	*get_env_value(const char *input, int *i)
 	env_value = getenv(temp);
 	return (env_value);
 }
-int	handle_double_quotes(const char **input, char *buffer, int *buf_index)
-{
-	(*input)++;
-	while (**input && **input != '"')
-	{
-		if (**input == '$' && ft_isalpha((*input)[1]))
-			handle_dollar_sign(input, buffer, buf_index);
-		else
-			buffer[(*buf_index)++] = *(*input)++;
-	}
-	(*input)++;
-	buffer[*buf_index] = '\0';
-	return (1);
-}
+// int	handle_double_quotes(const char **input, char *buffer, int *buf_index)
+// {
+// 	(*input)++;
+// 	while (**input && **input != '"')
+// 	{
+// 		if (**input == '$' && ft_isalpha((*input)[1]))
+// 			handle_dollar_sign(input, buffer, buf_index);
+// 		else
+// 			buffer[(*buf_index)++] = *(*input)++;
+// 	}
+// 	(*input)++;
+// 	buffer[*buf_index] = '\0';
+// 	return (1);
+// }
 
-int	handle_single_quotes(const char **input, char *buffer, int *buf_index)
-{
-	(*input)++;
-	while (**input && **input != '\'')
-		buffer[(*buf_index)++] = *(*input)++;
-	(*input)++;
-	return (0);
-}
+// int	handle_single_quotes(const char **input, char *buffer, int *buf_index)
+// {
+// 	(*input)++;
+// 	while (**input && **input != '\'')
+// 		buffer[(*buf_index)++] = *(*input)++;
+// 	(*input)++;
+// 	return (0);
+// }
 
 
 
